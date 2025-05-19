8a4ae4a4b822d89f359cc966a5c34eedb5e7a128
luizneves9 <luiz.neves9@gmail.com>
2025-05-19 10:53:03 -0300

    Apresentação-index.py

--- index.py
+++ index.py
@@ -13,8 +13,8 @@ from projeto_chatbot2 import (
 
 # --- Layout da Aplicação Streamlit ---
 
-st.title("Chatbot Alura - IA")
-st.write("Chatbot com informações da empresa para fins de conhecimentos.")
+st.title("Chatbot - Sua fonte completa de informações sobre a Alura")
+st.write("Pergunte abaixo e tire suas dúvidas instantaneamente")
 
 # --- Inicialização e Carregamento de Recursos com Cache ---
 # Usamos as funções importadas, MAS as envolvemos com os decoradores de cache do Streamlit.
